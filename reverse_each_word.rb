7796d8b28610440e70ca8de58da0dc883f6ce19c
Chris Cardle <cdcardle@gmail.com>
2017-10-28 01:15:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,12 @@
 def reverse_each_word(string)
   array = string.split
-  reversed_array = []
   array.each do |string|
     string.reverse!
   end
   return array.join(" ")
 end
 
-def reverse_each_word(string)
-  array = string.split
-  reversed_array = []
+def test(string)
   array.collect do |string|
     string.reverse
   end
